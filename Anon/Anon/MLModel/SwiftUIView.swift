1c178a7c0d9e4137ea4d5342d21e1ae52b99b42c
김재윤/Eddey <jaeyoon7421@naver.com>
2025-08-24 00:05:38 +0900

    feat: csv파일 변경

---
+++ Anon/Anon/MLModel/SwiftUIView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  Anon
+//
+//  Created by 김재윤 on 8/23/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    
+    private let model: ANON
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
